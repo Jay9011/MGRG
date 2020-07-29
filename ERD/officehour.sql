698824c7d4775bc657fe2db9359e78a31fc0e53f
skuley <skuleyandpotao@gmail.com>
2020-07-29 17:28:10 +0900

    chart 만들 sql 연습

--- ERD/officehour.sql
+++ ERD/officehour.sql
@@ -65,6 +65,46 @@ VALUES
  * "이번달" 직원 "출근/지각/결근/조퇴" 를 다 합하여 
  *  컬럼으로 출근 = # 지각 = # 결근 = # 이런식으로 뽑아야한다
  */
+SELECT * FROM OFFICE_HOUR;
 
-CREATE OR REPLACE VIEW statusChart AS
-SELECT 
+SELECT COUNT(CASE 
+			 WHEN STAT = '출근' THEN '출근' END ) AS intime, 
+	   COUNT(CASE 
+			 WHEN STAT = '퇴근' THEN '퇴근' END ) AS off, 
+	   COUNT(CASE 
+			 WHEN STAT = '조퇴' THEN '조퇴' END ) AS earlyOff, 
+	   COUNT(CASE 
+			 WHEN STAT = '지각' THEN '지각' END ) AS late
+FROM (SELECT  
+		h.*,
+		CASE
+			WHEN h.endTime IS NOT NULL THEN '퇴근'
+			WHEN h.endTime < 1800 THEN '조퇴'
+			WHEN h.startTime IS NULL THEN '미출근'
+			WHEN h.startTime <= 900 THEN '출근'
+			WHEN h.startTime <= 930 THEN '지각'
+			ELSE '결근'
+			END AS stat
+	FROM (SELECT oh.*, TO_NUMBER(TO_CHAR(oh.W_START , 'hh24mi')) AS startTime, 
+				 to_number(TO_CHAR(oh.W_END , 'hh24mi')) AS endTime
+		  FROM OFFICE_HOUR oh 
+		  WHERE (oh.W_START BETWEEN TRUNC(SYSDATE, 'mm') AND TRUNC(SYSDATE, 'dd')) AND oh.EMP_UID = 7) h)
+		  ;
+		  
+-- 이번 달 주말 제외한 모든 날짜의 출근 현황 뽑기
+SELECT *
+FROM (SELECT mon.*
+      FROM (SELECT TRUNC(SYSDATE ,'mm') + LEVEL - 1 AS IN_DATE
+         FROM DUAL CONNECT BY LEVEL <= SYSDATE - TRUNC(SYSDATE,'mm')) mon
+      WHERE TO_CHAR(mon.IN_DATE, 'd') NOT IN (1, 7)) work_day
+   LEFT OUTER JOIN (SELECT * FROM OFFICE_HOUR WHERE EMP_UID = 4) oh 
+   ON oh.W_START BETWEEN work_day.in_date AND work_day.in_date + 1
+;
+
+-- 휴일 뺀 이번 월 모든 날짜 뽑기
+SELECT mon.*
+FROM (SELECT TRUNC(SYSDATE ,'mm') + LEVEL - 1 AS IN_DATE
+   FROM DUAL CONNECT BY LEVEL <= SYSDATE - TRUNC(SYSDATE,'mm')) mon
+WHERE TO_CHAR(mon.IN_DATE, 'd') NOT IN (1, 7)
+;
+
