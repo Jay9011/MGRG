99da0ec5875273d4a217b30aab205db6d0aee147
tnsu <gks8270@gmail.com>
2020-08-04 13:37:24 +0900

    js

--- src/main/webapp/resources/JS/officehour/currenthr.js
+++ src/main/webapp/resources/JS/officehour/currenthr.js
@@ -21,8 +21,9 @@ $(document).ready(function(){
 			$('#startDate').datepicker('option', 'maxDate', selectedDate);
 		}
 	});
+	
 });
-
 $(document).on('keyup','.upnum', function() {
-	alert('dd');
-})
+	$(this).val( $(this).val().replace(/[^0-9]/g, "")
+			.replace(/^((19|20)\d{2})(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[0-1])([1-9]|[01][0-9]|2[0-3])([0-5][0-9])$/,"$1-$2-$3 $4:$5:$6"));
+});
